2f977a21206a8845f072a30ece6e346e9a308786
Todor Davchev <t.davchev@gmail.com>
2016-06-24 15:53:26 +0100

    updating spawn job session

--- job.sh
+++ job.sh
@@ -8,9 +8,12 @@ cd /home/s1045064/dissertation/repo-diss/sentence-classification/
 source /home/s1045064/dissertation/venv/bin/activate
 
 #THEANO_FLAGS="device=gpu$MLP_GPU" python /home/s1045064/dissertation/repo-diss/sentence-classification/conv_net_sentence.py -static -word2vec #$MLP_NH $MLP_R
-if [ $MODEL -eq 0] then
+if [ $MODEL -eq 1 ]; then
 	echo "$BATCH_SIZE, $DROPOUT, $CNL"
+        echo "Single Sentence Case"
 	THEANO_FLAGS="device=gpu$GPU_NO" python /home/s1045064/dissertation/repo-diss/sentence-classification/conv_net_sentence_singleCNN.py -static -word2vec $BATCH_SIZE $DROPOUT $CNL
 else
+        echo "$BATCH_SIZE_F, $DROPOUT_F, $CNL_F, $BATCH_SIZE_S, $DROPOUT_S, $CNL_F"
+        echo "Separated Sentences Case"
 	THEANO_FLAGS="device=gpu$GPU_NO" python /home/s1045064/dissertation/repo-diss/sentence-classification/conv_net_sentence.py -static -word2vec $BATCH_SIZE_F $DROPOUT_F $CNL_F $BATCH_SIZE_S $DROPOUT_S $CNL_S
 fi
