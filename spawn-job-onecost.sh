feeeb2db93cd7a54ab72b251f223cdc9cf9632f3
Todor Davchev <t.davchev@gmail.com>
2016-06-28 21:41:47 +0100

    Grid search

--- spawn-job-onecost.sh
+++ spawn-job-onecost.sh
@@ -1,12 +1,24 @@
 #!/bin/bash:
-for bs in 100 150 200; do  #10 25 50 100 200; do
-   for dropout in 0.5 0.2 0.0; do
-	qsub -v MLP_R=$1,MLP_GPU=$2 run.sh
-       for cnl in "relu" "tanh"; do
-          echo "Starting job with batch size: $bs, dropout: $dropout, conv_non_linear: $cnl"
-echo "Starting Model 2"
-if [ $5 -eq 1 ]; then
-	qsub -v BATCH_SIZE_F=$1,DROPOUT_F=$2,CNL_F=$3,GPU_NO=$4 job3.sh
-else
-	qsub -v BATCH_SIZE_F=$1,DROPOUT_F=$2,CNL_F=$3,GPU_NO=$4,MODE=$6,LR_DECAY=$7 job4.sh
-fi
+GPU=0
+for bs in 150 200 250; do  #10 25 50 100 200; do
+   for dropout in 0.5 0.2 0.1 0.0; do
+        for cnl in "relu" "tanh"; do
+        	for lr_decay in 0.9 0.95 1; do
+        		for mode in add mul; do
+        			GPU=$((GPU+1))
+        			if [ $GPU -eq 2 ]; then
+        				GPU=0
+        			fi
+		          	echo "Starting job with batch size: $bs, dropout: $dropout, conv_non_linear: $cnl"
+		          	if [ $1 -eq 1 ]; then
+		          		echo "Starting Model 2 with 3 CNNs"
+						qsub -v BATCH_SIZE_F=$1,DROPOUT_F=$2,CNL_F=$3,GPU_NO=$4 job3.sh
+					else
+						echo "Starting Model 3 with MODE $6"
+						qsub -v BATCH_SIZE_F=bs,DROPOUT_F=dropout,CNL_F=cnl,GPU_NO=GPU,MODE=mode,LR_DECAY=lr_decay job4.sh
+					fi
+				done
+			done
+		done
+	done
+done
