cb7efcdb10b4a1df8f04ab9277b151631e092660
Todor Davchev <t.davchev@gmail.com>
2016-06-29 16:45:48 +0100

    randomising parameter optimization

--- spawn-job-onecost.sh
+++ spawn-job-onecost.sh
@@ -1,87 +1,88 @@
 #!/bin/bash:
-GPU=0
-cnl="relu"
-lr_decay=0.95
-mode="add"
-for bs in 50; do  #150 250; do
-   for dropout in 0.2 ; do # 0.5 0.1 0.0
-		GPU=$((GPU+1))
-		if [ $GPU -eq 3 ]; then
-			GPU=0
-		fi
-      	if [ $1 -eq 1 ]; then
-      		echo "Starting Model 2 with 3 CNNs"
-			qsub -v BATCH_SIZE_F=$1,DROPOUT_F=$2,CNL_F=$3,GPU_NO=$4 job3.sh
-		else
-			# if [ $lr_decay != 0.9 ] && [ $cnl != "relu" ] && [ $mode != "mul" ]; then
-			# 	echo "tuk sum"
-			# 	echo "$cnl $lr_decay $mode"
-			# 		echo "sega tuk"
-			# 		echo "$cnl $lr_decay $mode"
-			# 		if [ $lr_decay != 0.95 ] && [ $cnl != "relu" ] && [ $mode != "mul" ]; then
-			# 			echo "i sega tuk"
-			# 			echo "$cnl $lr_decay $mode"
-			# 			if [ $lr_decay != 0.9 ] && [ $cnl != "tanh" ] && [ $mode != "mul" ]; then
-			cnl="relu"
-			lr_decay=0.95
-			mode="add"
-			echo "Starting job with batch size: $bs, dropout: $dropout, conv_non_linear: $cnl lr_decay: $lr_decay in mode: $mode on GPU: $GPU"
-			echo "Starting Model 3 with MODE $mode"
-			# FIRST='qsub -v BATCH_SIZE_F=$bs,DROPOUT_F=$dropout,CNL_F=$cnl,GPU_NO=$GPU,MODE=$mode,LR_DECAY=$lr_decay job4.sh'
-			# echo $FIRST
-			qsub -v BATCH_SIZE_F=$bs,DROPOUT_F=$dropout,CNL_F=$cnl,GPU_NO=$GPU,MODE=$mode,LR_DECAY=$lr_decay job4.sh
-			cnl="tanh"
-			lr_decay=0.95
-			mode="add"
-			echo "Starting job with batch size: $bs, dropout: $dropout, conv_non_linear: $cnl lr_decay: $lr_decay in mode: $mode on GPU: $GPU"
-			echo "Starting Model 3 with MODE $mode"
-			# SECOND='qsub -v BATCH_SIZE_F=$bs,DROPOUT_F=$dropout,CNL_F=$cnl,GPU_NO=$GPU,MODE=$mode,LR_DECAY=$lr_decay job4.sh'
-			# echo $SECOND
-			qsub -v BATCH_SIZE_F=$bs,DROPOUT_F=$dropout,CNL_F=$cnl,GPU_NO=$GPU,MODE=$mode,LR_DECAY=$lr_decay job4.sh
-			cnl="tanh"
-			lr_decay=0.95
-			mode="mul"
-			echo "Starting job with batch size: $bs, dropout: $dropout, conv_non_linear: $cnl lr_decay: $lr_decay in mode: $mode on GPU: $GPU"
-			echo "Starting Model 3 with MODE $mode"
-			# THIRD='qsub -v BATCH_SIZE_F=$bs,DROPOUT_F=$dropout,CNL_F=$cnl,GPU_NO=$GPU,MODE=$mode,LR_DECAY=$lr_decay job4.sh'
-			qsub -v BATCH_SIZE_F=$bs,DROPOUT_F=$dropout,CNL_F=$cnl,GPU_NO=$GPU,MODE=$mode,LR_DECAY=$lr_decay job4.sh
-			# echo $THIRD
-			cnl="tanh"
-			lr_decay=0.94
-			mode="mul"
-			echo "Starting job with batch size: $bs, dropout: $dropout, conv_non_linear: $cnl lr_decay: $lr_decay in mode: $mode on GPU: $GPU"
-			echo "Starting Model 3 with MODE $mode"
-			qsub -v BATCH_SIZE_F=$bs,DROPOUT_F=$dropout,CNL_F=$cnl,GPU_NO=$GPU,MODE=$mode,LR_DECAY=$lr_decay job4.sh
-			# FOURTH='qsub -W depend=afterok:$FIRST -v BATCH_SIZE_F=$bs,DROPOUT_F=$dropout,CNL_F=$cnl,GPU_NO=$GPU,MODE=$mode,LR_DECAY=$lr_decay job4.sh'
-			# echo $FOURTH
-			cnl="tanh"
-			lr_decay=0.94
-			mode="add"
-			echo "Starting job with batch size: $bs, dropout: $dropout, conv_non_linear: $cnl lr_decay: $lr_decay in mode: $mode on GPU: $GPU"
-			echo "Starting Model 3 with MODE $mode"
-			# FIFTH='qsub -W depend=afterok:$SECOND -v BATCH_SIZE_F=$bs,DROPOUT_F=$dropout,CNL_F=$cnl,GPU_NO=$GPU,MODE=$mode,LR_DECAY=$lr_decay job4.sh'
-			qsub -v BATCH_SIZE_F=$bs,DROPOUT_F=$dropout,CNL_F=$cnl,GPU_NO=$GPU,MODE=$mode,LR_DECAY=$lr_decay job4.sh
-			# echo $FIFTH
-			cnl="relu"
-			lr_decay=0.94
-			mode="add"
-			echo "Starting job with batch size: $bs, dropout: $dropout, conv_non_linear: $cnl lr_decay: $lr_decay in mode: $mode on GPU: $GPU"
-			echo "Starting Model 3 with MODE $mode"
-			# SIXTH='qsub -W depend=afterok:$THIRD -v BATCH_SIZE_F=$bs,DROPOUT_F=$dropout,CNL_F=$cnl,GPU_NO=$GPU,MODE=$mode,LR_DECAY=$lr_decay job4.sh'
-			qsub -v BATCH_SIZE_F=$bs,DROPOUT_F=$dropout,CNL_F=$cnl,GPU_NO=$GPU,MODE=$mode,LR_DECAY=$lr_decay job4.sh
-			# echo $SIXTH
-		    cnl="relu"
-			lr_decay=0.94
-			mode="mul"
-			echo "Starting job with batch size: $bs, dropout: $dropout, conv_non_linear: $cnl lr_decay: $lr_decay in mode: $mode on GPU: $GPU"
-			echo "Starting Model 3 with MODE $mode"
-			qsub -v BATCH_SIZE_F=$bs,DROPOUT_F=$dropout,CNL_F=$cnl,GPU_NO=$GPU,MODE=$mode,LR_DECAY=$lr_decay job4.sh
-			# SEVENTH='qsub -v BATCH_SIZE_F=$bs,DROPOUT_F=$dropout,CNL_F=$cnl,GPU_NO=$GPU,MODE=$mode,LR_DECAY=$lr_decay job4.sh'
-			# echo $SEVENTH
-			# 			fi
-			# 		fi
-			# 	fi
-			# fi					
-		fi
+GPU=0  #initialize
+cnl="relu"  #initialize
+lr_decay=0.95  #initialize
+mode="add" #initializea
+echo "$lr_decay"
+function randomNum {
+	# $RANDOM returns a different random integer at each invocation.
+	# Nominal range: 0 - 32767 (signed 16-bit integer).
+	let "RANGE=$1"
+	echo
+
+	let "FLOOR=$2"
+
+	# Let's generate a random number within a given range
+	number=0   #initialize
+	while [ "$number" -le $FLOOR ]
+	do
+  		number=$RANDOM
+  		let "number %= $RANGE"  # Scales $number down within $RANGE.
 	done
+	return $number
+}
+
+function randomBinary {
+# Generate binary choice, that is, "true" or "false" value.
+BINARY=2
+T=1
+number=$RANDOM
+
+	let "number %= $BINARY"
+	#  Note that    let "number >>= 14"    gives a better random distribution
+	#+ (right shifts out everything except last binary digit).
+	if [ "$number" -eq $T ]
+	then
+  		return "Relu"
+	else
+  		return "FALSE"
+	fi
+
+	echo
+}
+
+function activationNum {
+	# Generate a on random an initialisation.
+	SPOTS=4   # Modulo 6 gives range 0 - 3.
+    # Incrementing by 1 gives desired range of 1 - 4.
+	die1=0
+	# Would it be better to just set SPOTS=7 and not add 1? 
+	# Tosses each die separately, and so gives correct odds.
+
+    	let "die1 = $RANDOM % $SPOTS +1" # Roll first one.
+
+	let "throw = $die1"
+	return $throw
+}
+
+count=0
+bs=50
+while [ $count -le 6 ]
+do
+	GPU=$((GPU+1))
+	if [ $GPU -eq 3 ]; then
+		GPU=0
+	fi
+	let "count=$count+1"
+	randomNum 100 10
+	dropout=$?
+	echo "Dropout is --- $dropout"
+	randomNum 100 10
+	lr_decay=$?
+	echo "Random number for lr_decay --- $lr_decay"
+	randomNum 100 10
+	alpha=$?
+	let "beta=100-$alpha"
+	echo "Setting alpha and beta to --- $alpha, $beta"
+	activationNum
+	activation=$?
+	echo "Activation num: $activation"
+	randomNum 10 1
+	sqr_norm_lim=$?
+	echo "sqr_norm_lim --- $sqr_norm_lim"
+	echo "Starting job with batch size: $bs, dropout: $dropout, conv_non_linear: $cnl lr_decay: $lr_decay in mode: $mode on GPU: $GPU"
+	echo "Starting Model 3 with MODE $mode"
+	qsub -v BATCH_SIZE_F=$bs,DROPOUT_F=$dropout,CNL_F=$cnl,GPU_NO=$GPU,MODE=$mode,LR_DECAY=$lr_decay,ALPHA=$alpha,BETA=$beta,ACTIVATION=$activation,SQR_NORM_LIM=$sqr_norm_lim job4.sh
 done
+
+exit 0
