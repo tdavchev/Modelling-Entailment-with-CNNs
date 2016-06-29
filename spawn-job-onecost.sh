214b99e8be56875fe1887081c25b7efa21052e6b
Todor Davchev <t.davchev@gmail.com>
2016-06-29 10:43:12 +0100

    script re-run

--- spawn-job-onecost.sh
+++ spawn-job-onecost.sh
@@ -3,8 +3,8 @@ GPU=0
 for bs in 50; do  #150 250; do
    for dropout in 0.2 ; do # 0.5 0.1 0.0
         for cnl in "relu" "tanh"; do
-        	for lr_decay in 0.9 0.95 1.0; do
-        		for mode in add mul; do
+        	for lr_decay in 0.9 0.95 0.94; do
+        		for mode in "add" "mul"; do
         			GPU=$((GPU+1))
         			if [ $GPU -eq 3 ]; then
         				GPU=0
@@ -14,8 +14,16 @@ for bs in 50; do  #150 250; do
 		          		echo "Starting Model 2 with 3 CNNs"
 						qsub -v BATCH_SIZE_F=$1,DROPOUT_F=$2,CNL_F=$3,GPU_NO=$4 job3.sh
 					else
-						echo "Starting Model 3 with MODE $mode"
-						qsub -v BATCH_SIZE_F=$bs,DROPOUT_F=$dropout,CNL_F=$cnl,GPU_NO=$GPU,MODE=$mode,LR_DECAY=$lr_decay job4.sh
+						if [ $lr_decay -neq 0.9 ] && [ $cnl -neq "relu" ] && [ $mode -neq "mul" ]; then
+							if [ $lr_decay -neq 0.9 ] && [ $cnl -neq "relu" ] [ $mode -neq "add" ]; then
+								if [ $lr_decay -neq 0.95 ] && [ $cnl -neq "relu" ] [ $mode -neq "mul" ]; then
+									if [ $lr_decay -neq 0.9 ] && [ $cnl -neq "tanh" ] && [ $mode -neq "mul" ]; then
+										echo "Starting Model 3 with MODE $mode"
+										qsub -v BATCH_SIZE_F=$bs,DROPOUT_F=$dropout,CNL_F=$cnl,GPU_NO=$GPU,MODE=$mode,LR_DECAY=$lr_decay job4.sh
+									fi
+								fi
+							fi
+						fi					
 					fi
 				done
 			done
