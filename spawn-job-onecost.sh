90f264a297577cf10d6922ab6c15c4363dd05ed5
Todor Davchev <t.davchev@gmail.com>
2016-07-03 15:05:09 +0100

    spawning jobs updt

--- spawn-job-onecost.sh
+++ spawn-job-onecost.sh
@@ -58,42 +58,52 @@ function activationNum {
 count=0
 bs=50
 #id=`python select_gpu.py`
-while [ $count -le 9 ]
+while [ $count -le 10 ]
 do
 	GPU=$((GPU+1))
 	if [ $GPU -eq 3 ]; then
 		GPU=0
 	fi
 	let "count=$count+1"
-	randomNum 20 10
+	randomNum 40 10
 	num=$?
 	mode="add"
-	if [ $num -le 15 ]; then
+	if [ $num -le 10 ]; then
 		mode="mul"
+	elif [ $num -le 20 ]; then
+		mode="concat"
+	elif [ $num -le 30 ]; then
+		mode="circ"
 	fi
-	mode="add"
+	#mode="add"
         randomNum 20 10
         num=$?
 	cnl="tanh"
 	if [ $num -le 15 ]; then
 		cnl="relu"
 	fi
+	cnl="relu"
 	randomNum 50 20
+	#dropout=20
 	dropout=$?
 	echo "Dropout is --- $dropout"
 	randomNum 94 85
 	lr_decay=$?
+	#lr_decay=95
 	echo "Random number for lr_decay --- $lr_decay"
-	randomNum 70 10
+	randomNum 50 30
 	alpha=$?
+	#alpha=40
 	#alpha=100
-	let "beta=200-$alpha"
+	let "beta=100-$alpha"
 	echo "Setting alpha and beta to --- $alpha, $beta"
 	activationNum
 	activation=$?
+	#activation=3
 	echo "Activation num: $activation"
 	randomNum 10 6
 	sqr_norm_lim=$?
+	#sqr_norm_lim=9
 	echo "sqr_norm_lim --- $sqr_norm_lim"
 	echo "Starting job with batch size: $bs, dropout: $dropout, conv_non_linear: $cnl lr_decay: $lr_decay in mode: $mode on GPU: $GPU"
 	echo "Starting Model 3 with MODE $mode"
