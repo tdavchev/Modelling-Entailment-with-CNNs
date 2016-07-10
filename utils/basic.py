9216d31bcd3846d793fcc808975154508c020d01
Todor Davchev <t.davchev@gmail.com>
2016-07-10 09:09:40 +0100

    normal size data

--- utils/basic.py
+++ utils/basic.py
@@ -31,7 +31,7 @@ def make_idx_data(revs, word_idx_map, max_l=81, k=300, filter_h=5):
     test = np.array(test,dtype="int")
     valid = np.array(valid,dtype="int")
 
-    return [train,  test, valid]
+    return [train, test, valid]
 
 def make_idx_data_cv(revs, word_idx_map, cv, max_l=51, k=300, filter_h=5):
     """
@@ -47,7 +47,7 @@ def make_idx_data_cv(revs, word_idx_map, cv, max_l=51, k=300, filter_h=5):
             train.append(sent)   
     train = np.array(train,dtype="int")
     test = np.array(test,dtype="int")
-    return [train[:10], test[:10]] 
+    return [train, test] 
 
 def set_layer0_input(Words, img_h, test_size, x):
     test_layer0_input_one = Words[T.cast(x.flatten(),dtype="int32")].reshape((test_size,1,img_h,Words.shape[1]))
