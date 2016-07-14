f25469dc154ce9888dd7bba937d507752eef246f
Todor Davchev <t.davchev@gmail.com>
2016-07-14 01:06:15 +0100

    fix

--- utils/arithmetics.py
+++ utils/arithmetics.py
@@ -86,8 +86,7 @@ def mix2(layer1_inputs,batch_size,alpha,beta,concat):
     lista.append(layer1_sub)
     layer1_input = T.concatenate(lista,1) # [50 900]
 
-    # layer1_mul = mul(concat) # [50 300]
-    layer1_add = add(batch_size, alpha, beta, concat) # [50 300]
+    layer1_mul = mul(concat) # [50 300]
 
     lista = []
     lista.append(layer1_input)
