6bee4ad9543f1a9ec2dd85217c59a31f2a36f37e
Todor Davchev <t.davchev@gmail.com>
2016-07-23 10:50:51 +0100

    hm

--- utils/arithmetics.py
+++ utils/arithmetics.py
@@ -154,21 +154,14 @@ def mix7(layer1_inputs,batch_size,alpha,beta,concat):
     layer1_concat = concatenate_tensors(layer1_inputs)# [50 600] 
     layer1_add = add(batch_size, alpha, beta, concat) # [50 300] 
     layer1_sub = sub(batch_size, alpha, beta, concat) # [50 300]
-    layer1_circ = circular_convolution(layer1_inputs) # [50 300]
+    layer1_mul = mul(concat) # [50 300]
+    # layer1_circ = circular_convolution(layer1_inputs) # [50 300]
 
     lista = []
     lista.append(layer1_concat)
     lista.append(layer1_add)
-    # layer1_input = T.concatenate(lista,1) # [50 900]
-
-    # lista = []
-    # lista.append(layer1_input)
     lista.append(layer1_sub)
-    # layer1_input = T.concatenate(lista,1) # [50 1200]
-
-    # lista = []
-    # lista.append(layer1_input)
-    lista.append(layer1_circ)
+    lista.append(layer1_mul)
 
     return T.concatenate(lista,1) # [50 1500]  
 
